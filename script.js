74f25ce92416eae4279a5ddb9e6a8181fc7a336e
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-08-02 10:47:28 -0300

    Adicionando o script

---
+++ script.js
@@ -0,0 +1,12 @@
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+  [modal, fade].forEach((elemento) => elemento.classList.toggle('hide'));
+}
+
+[openModalButton, closeModalButton].forEach((elemento) => {
+  elemento.addEventListener("click", () => toggleModal());
+});
